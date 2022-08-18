ec415df339532cbce0af1a0a0a7c9c88b3c0e60c
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-08-18 15:47:19 +0900

    signUpForm test 에러 수정

--- src/test/java/com/study3355/account/AccountControllerTest.java
+++ src/test/java/com/study3355/account/AccountControllerTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -20,9 +22,11 @@ class AccountControllerTest {
 
     @Test
     @DisplayName("회원 가입 화면 보이는지 테스트")
+    @WithMockUser // 401 UnAuthorized 에러 방지
     void signUpForm() throws Exception {
 
-        mockMvc.perform(get("/sign-up"))
+        mockMvc.perform(get("/sign-up")
+                        .accept(MediaType.TEXT_HTML))
                 .andExpect(status().isOk())
                 .andExpect(view().name("account/sign-up"))
                 .andExpect(model().attributeExists("signUpForm"));
