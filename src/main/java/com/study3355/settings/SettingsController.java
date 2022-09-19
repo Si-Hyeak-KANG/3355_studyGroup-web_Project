25375bcc6914546ffd5562160ab1061b2d2ba19f
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-09-20 00:01:50 +0900

    관심사 삭제 기능

--- src/main/java/com/study3355/settings/SettingsController.java
+++ src/main/java/com/study3355/settings/SettingsController.java
@@ -154,7 +154,7 @@ public class SettingsController {
         return SETTINGS_TAGS_VIEW_NAME;
     }
 
-    @PostMapping("/settings/tags/add")
+    @PostMapping(SETTINGS_TAGS_URL + "/add")
     @ResponseBody
     public ResponseEntity addTag(@CurrentUser Account account, @RequestBody TagForm tagForm) {
         String title = tagForm.getTagTitle();
@@ -165,6 +165,19 @@ public class SettingsController {
                                 .build()));
 
         accountService.addTag(account, tag);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping(SETTINGS_TAGS_URL + "/remove")
+    @ResponseBody
+    public ResponseEntity removeTag(@CurrentUser Account account, @RequestBody TagForm tagForm) {
+        String title = tagForm.getTagTitle();
+        Tag tag = tagRepository.findByTitle(title).get();
+        if(tag == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        accountService.removeTag(account,tag);
         return ResponseEntity.ok().build();
     }
 }
